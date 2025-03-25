33d56fdeaf3f33c9ce8ce3b51b75b778e6fa8e5c
PRASADD <durgaaprasadd765@gmail.com>
2025-03-25 18:58:45 +0530

    Update Login.tsx

--- src/Login.tsx
+++ src/Login.tsx
@@ -58,7 +58,7 @@ const Login: React.FC = () => {
   const handleLogin = async () => {
     setLoading(true);
     try {
-      const response = await axios.post('http://18.61.74.17:8080/api/login', {
+      const response = await axios.post('http://18.60.107.83:8080/api/login', {
         firstName,
         password,
       });
